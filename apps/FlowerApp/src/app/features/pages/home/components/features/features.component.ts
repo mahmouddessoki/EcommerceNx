6a587edd4b82c4bd59a8b617ee294c299650bec0
mahmouddessoki <mahmouddessoki33@gmail.com>
2025-05-20 02:17:43 +0300

    features merge request

--- apps/FlowerApp/src/app/features/pages/home/components/features/features.component.ts
+++ apps/FlowerApp/src/app/features/pages/home/components/features/features.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+interface feature {
+  icon : string ,
+  name : string ,
+  label : string
+}
 @Component({
   selector: 'app-features',
   imports: [CommonModule],
   templateUrl: './features.component.html',
-  styleUrl: './features.component.css',
+  styleUrl: './features.component.scss',
 })
-export class FeaturesComponent {}
+export class FeaturesComponent {
+
+  features : feature[] = [
+    {
+      icon : 'pi pi-truck',
+      name : 'free delivery',
+      label : 'Orders Over $120'
+    },
+    {
+      icon : 'pi pi-sync',
+      name : 'Get Refund',
+      label : 'Within 30 Days Returns'
+    },
+    {
+      icon : 'pi pi-wallet',
+      name : 'Safe Payment',
+      label : '100% Secure Payment'
+    },
+    {
+      icon : 'pi pi-headphones',
+      name : '24/7 Support',
+      label : 'Feel Free To Call Us'
+    }
+  ]
+
+
+
+}
--- apps/FlowerApp/src/app/features/pages/home/home.component.ts
+++ apps/FlowerApp/src/app/features/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FeaturesComponent } from "./components/features/features.component";
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule, FeaturesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
