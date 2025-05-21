cdbca6c28fe6ec0fdbe100b2b93e062c3932ea51
mahmouddessoki <mahmouddessoki33@gmail.com>
2025-05-22 02:55:34 +0300

    gifts-completed

--- apps/FlowerApp/src/app/features/pages/components/specialGifts/specialGifts.component.ts
+++ apps/FlowerApp/src/app/features/pages/components/specialGifts/specialGifts.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GiftCardComponent } from "../gift-card/gift-card.component";
+import { CarouselModule } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
+import { TagModule } from 'primeng/tag';
 export interface gift {
   title:string,
   name : string ,
   btnLabel : string,
 }
+export interface carouselItem {
+  title:string,
+  name : string ,
+  desc:string,
+  btnLabel : string,
+  img:string
+}
 @Component({
   selector: 'app-special-gifts',
-  imports: [CommonModule, GiftCardComponent],
+  imports: [CommonModule, GiftCardComponent,CarouselModule, ButtonModule, TagModule],
   templateUrl: './specialGifts.component.html',
   styleUrl: './specialGifts.component.scss',
 })
@@ -30,6 +40,44 @@ export class SpecialGiftsComponent {
       title: 'Occasion Gifts',
       name: 'Combo Sets Gift Box Up To 50% Off',
       btnLabel: 'Discover Now'
+    },
+  ]
+  bannerItems:carouselItem[] = [
+    {
+      title:"Best Gift Shop",
+      name:"Choose Perfect Gifts From Us",
+      btnLabel:"shop now",
+      desc:`Culpa ducimus nesciunt aliquam
+       non rerum esse recusandae omnis.
+        Rerum optio dolores et.`,
+        img:"/images/gifts/gift4.jpg"
+    },
+    {
+      title:"Best Gift Shop",
+      name:"Choose Perfect Gifts From Us",
+      btnLabel:"shop now",
+      desc:`Culpa ducimus nesciunt aliquam
+       non rerum esse recusandae omnis.
+        Rerum optio dolores et.`,
+        img:"/images/gifts/gift4.jpg"
+    },
+    {
+      title:"Best Gift Shop",
+      name:"Choose Perfect Gifts From Us",
+      btnLabel:"shop now",
+      desc:`Culpa ducimus nesciunt aliquam
+       non rerum esse recusandae omnis.
+        Rerum optio dolores et.`,
+        img:"/images/gifts/gift4.jpg"
+    },
+    {
+      title:"Best Gift Shop",
+      name:"Choose Perfect Gifts From Us",
+      btnLabel:"shop now",
+      desc:`Culpa ducimus nesciunt aliquam
+       non rerum esse recusandae omnis.
+        Rerum optio dolores et.`,
+        img:"/images/gifts/gift4.jpg"
     },
   ]
 }
